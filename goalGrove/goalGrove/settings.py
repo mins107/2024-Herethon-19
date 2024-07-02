57a3713cbcedd85d02cd3f8f5c04718752c9ec7a
HeewonJo <heewonjo.me@gmail.com>
2024-07-02 10:00:16 +0900

    Feat: 회원 가입 기능 수정"
    폼 전달 정상 실행, 오류 메시지 출력

--- goalGrove/goalGrove/settings.py
+++ goalGrove/goalGrove/settings.py
@@ -129,5 +129,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.user'
 
-AUTH_USER_MODEL = 'users.User'
+# etc
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
