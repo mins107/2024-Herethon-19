f4a02464f8502c1cbd423d06de1d027994b19505
mhdiree <mhdiree@sookmyung.ac.kr>
2024-07-03 11:12:09 +0900

    posts 수정

--- goalGrove/posts/views.py
+++ goalGrove/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from .models import Post
 from .forms import PostForm
+from django.contrib import messages
 
 def create_post(request):
     if request.method == 'POST':
@@ -16,5 +17,19 @@ def create_post(request):
     return render(request, 'posts/create_post.html', {'form': form})
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'posts/post_list.html', {'posts': posts})
+    if request.method == "GET":
+        form = PostForm()
+        return render(request, 'posts/post_list.html', {'form': form})
+    
+    form = PostForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        messages.success(request, '완료')
+        return redirect('post_list')
+    
+    return render(request, 'posts/post_list.html', {'form': form})
+
+
+#def post_list(request):
+#    posts = Post.objects.all()
+#    return render(request, 'posts/post_list.html', {'posts': posts})
