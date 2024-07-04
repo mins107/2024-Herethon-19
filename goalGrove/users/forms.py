12f90835d1dcdf5b173776c960041806e28ebeca
HeewonJo <heewonjo.me@gmail.com>
2024-07-04 13:22:10 +0900

    Feat: "전체 수정"

--- goalGrove/users/forms.py
+++ goalGrove/users/forms.py
@@ -1,13 +1,30 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 class SignUpForm(UserCreationForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'email', 'password1', 'password2', 'nickname']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'nickname': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+class ResetPasswordForm(forms.Form):
+    new_password = forms.CharField(label='새 비밀번호', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='새 비밀번호 확인', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password and confirm_password:
+            if new_password != confirm_password:
+                raise forms.ValidationError("새 비밀번호와 비밀번호 확인이 일치하지 않습니다.")
+        
+        return cleaned_data
--- goalGrove/users/urls.py
+++ goalGrove/users/urls.py
@@ -5,7 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
     path('pwverification/', pwverification_view, name='pwverification'),
     path('pwreset/', pwreset_view, name='pwreset'),
 ]
