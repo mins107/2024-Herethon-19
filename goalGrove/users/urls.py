85fb93d85e1260b2ddce0787c7b7998bec6648cb
HeewonJo <heewonjo.me@gmail.com>
2024-07-05 13:47:46 +0900

    Feat: "Shop 수정"

--- goalGrove/users/urls.py
+++ goalGrove/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 app_name = 'users'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('main/', main_view, name='main'),
     path('shop/', shop_view, name='shop'),
     path('signup/', signup_view, name='signup'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('pwverification/', pwverification_view, name='pwverification'),
     path('pwreset/', pwreset_view, name='pwreset'),
 ]
