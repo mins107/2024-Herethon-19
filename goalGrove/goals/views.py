c38b1d65ddb5ca93f6582ccbf93ad0c452b85b1a
mhdiree <mhdiree@gmail.com>
2024-07-05 23:03:23 +0900

    goals 수정

--- goalGrove/goals/views.py
+++ goalGrove/goals/views.py
@@ -27,18 +27,3 @@ def goal_detail(request, goal_id):
     goal = get_object_or_404(Goal, id=goal_id, user=request.user)
     goal_data = {'id': goal.id, 'title': goal.title, 'description': goal.description}
     return JsonResponse(goal_data)
-
-@require_POST
-def add_shop_goal(request):
-    user = request.user
-    title = request.POST.get('title')
-    description = request.POST.get('description', '')
-    shop_goal = ShopGoal.objects.create(user=user, title=title, description=description)
-    return JsonResponse({'id': shop_goal.id, 'title': shop_goal.title, 'description': shop_goal.description})
-
-@require_GET
-def list_shop_goals(request):
-    user = request.user
-    shop_goals = ShopGoal.objects.filter(user=user)
-    shop_goals_data = [{'id': shop_goal.id, 'title': shop_goal.title, 'description': shop_goal.description} for shop_goal in shop_goals]
-    return JsonResponse(shop_goals_data, safe=False)
